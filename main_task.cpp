0752972ddf43cd97be55b26de36e97828c200acc
Praktik <praktik@localhost.localdomain>
2024-06-26 12:04:54 +0300

    Commit 3

--- main_task.cpp
+++ main_task.cpp
@@ -13,30 +13,42 @@ vector<uint16_t> H;
 vector<uint32_t> X;
 vector<uint32_t> Y;
 
+
+struct hxy
+{
+    uint16_t H;
+    uint32_t X;
+    uint32_t Y;
+}__attribute__((packed));
+
+vector<hxy> all_structs;
+
+hxy convert_new(char* data)
+{
+    return (*(reinterpret_cast<hxy*>(data)));
+}
+
 uint16_t convert(char end, char start)
 {
-    stringstream ss;
-    string str;
-    ss << bitset<8>(end) << bitset<8>(start);
-    str  = ss.str();
-    cout << "Line bits " << str << endl;
-    bitset<16> bit (str);
-    //cout << "bit.to_ulong() = "<< bit.to_ulong() << endl;
-    return uint16_t(bit.to_ulong());
+    uint16_t result = (end << 8) + start;
+    return result;
 }
 
 uint32_t convert_four(char fourth, char third, char second, char first)
 {
-    stringstream ss;
-    string str;
-    ss << bitset<8>(fourth) << bitset<8>(third) << bitset<8>(second) << bitset<8>(first);
-    str  = ss.str();
-    cout << "Line bits " << str << endl;
-    bitset<32> bit (str);
-    //cout << "bit.to_ulong() = "<< bit.to_ulong() << endl;
-    return uint32_t(bit.to_ulong());
+    uint32_t result = (fourth << 24) + (third << 16) + (second << 8) + first;
+    return result;
 }
 
+uint32_t convert_four_pointer(char* data)
+{
+    return (*(reinterpret_cast<uint32_t*>(data)));
+}
+
+uint16_t convert_foo(char* data)
+{
+    return (*(reinterpret_cast<uint16_t*>(data))); // reinterpret_cast< target-type >( expression )  Преобразует типы, переосмысливая базовый битовый шаблон ; Returns a value of type target-type. 
+}
 
 int main()
 {
@@ -69,19 +81,23 @@ int main()
             if (binary_data){cout << "STRUCT № " << dec << count+1 << " read successfully" << endl;}
             else {cout << "error: only " << binary_data.gcount() << " could be read" << endl;}
 
-            if (convert(buffer[1], buffer[0]) == uint16_t(4660))
+            if (convert_foo(buffer) == uint16_t(4660))
             {
-                Start.push_back(convert(buffer[1], buffer[0]));
-                H.push_back(convert(buffer[3], buffer[2]));
-                X.push_back(convert_four(buffer[7], buffer[6], buffer[5], buffer[4]));
-                Y.push_back(convert_four(buffer[11], buffer[10], buffer[9], buffer[8]));
-                End.push_back(convert(buffer[13], buffer[12]));
+                //convert_new(buffer+2);
+                all_structs.push_back(convert_new(buffer+2));
+                // Start.push_back(convert_foo(buffer));
+                // H.push_back(convert_foo(buffer+2));
+                // X.push_back(convert_four(buffer[7], buffer[6], buffer[5], buffer[4]));
+                // Y.push_back(convert_four(buffer[11], buffer[10], buffer[9], buffer[8]));
+                // // X.push_back(convert_four_pointer(buffer+4));
+                // // Y.push_back(convert_four_pointer(buffer+8));
+                // End.push_back(convert_foo(buffer+12));
 
-                cout << "Start[" << count <<"] = " << Start[count] << endl;
-                cout << "H[" << count <<"] = " << H[count] << endl;
-                cout << "X[" << count <<"] = " << X[count] << endl;
-                cout << "Y[" << count <<"] = " << Y[count] << endl;
-                cout << "End[" << count <<"] = " << End[count] << endl;
+                // cout << "Start[" << count <<"] = " << Start[count] << endl;
+                cout << "H[" << count <<"] = " << all_structs[count].H << endl;
+                cout << "X[" << count <<"] = " << all_structs[count].X << endl;
+                cout << "Y[" << count <<"] = " << all_structs[count].Y<< endl;
+                // cout << "End[" << count <<"] = " << End[count] << endl;
             }
 
             while (i < length_struct)
